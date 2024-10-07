352e46071182bb90b8929bf2adb0b390942019e4
Michael.Patino <mpatino@realogicworks.com>
2024-10-07 10:00:33 -0500

    First commit - VS

---
+++ stwarstudios.web.client/src/Components/Shared/NavbarScroller.tsx
@@ -0,0 +1,22 @@
+// import * as React from 'react'
+// import styled from 'styled-components';
+
+
+
+// const NavbarScroller = (props: {
+//     brand: { name: string; to: string },
+//     links: Array<{ name: string, to: string }>
+//   }) => {
+//     const { brand, links } = props;
+//     const NavLinks: any = () => links.map((link: { name: string, to: string }) => <Li key={link.name}><a href={link.to}>{link.name}</a></Li>);
+//     return (
+//       <Navbar>
+//         <Brand href={brand.to}>{brand.name}</Brand>
+//         <Ul>
+//           <NavLinks />
+//         </Ul>
+//       </Navbar >
+//     )
+//   };
+
+// export default NavbarScroller;
