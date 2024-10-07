c45ce5cc0309a789e254374accf35e44fcbd8da3
Michael.Patino <mpatino@realogicworks.com>
2024-10-07 14:21:42 -0500

    remove update vite

--- stwarstudios.web.client/src/App.tsx
+++ stwarstudios.web.client/src/App.tsx
@@ -1,28 +1,26 @@
-import 'vite/modulepreload-polyfill'
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'reset-css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './Components/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-function App()
-{ 
-    return (
-        <>
-        <div className="App">
-            <Router>
-                <div className="">
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={
-                            <HomePage/>
-                            }
-                        />
-                    </Routes>
-                </div>
-           </Router>
-      </div>
-      </>
-    );
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
