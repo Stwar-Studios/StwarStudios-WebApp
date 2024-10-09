671f11577b80602a1aab5d751cdaef05c78e37f5
Michael.Patino <michael.patino@stwarstudios.com>
2024-10-09 17:34:30 -0500

    remove using

--- StWarStudios.Web.Server/web-client/src/index.tsx
+++ StWarStudios.Web.Server/web-client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import homepage from './Components/homepage';
 
 
 const root = ReactDOM.createRoot(
