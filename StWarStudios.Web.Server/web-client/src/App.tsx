1eae3bc9360b2967106bc703a3d4c2635eea3f23
Michael.Patino <michael.patino@stwarstudios.com>
2024-10-09 10:45:07 -0500

    case naming

--- StWarStudios.Web.Server/web-client/src/App.tsx
+++ StWarStudios.Web.Server/web-client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'reset-css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './Components/HomePage';
+import HomePage from './Components/homepage';
 import NavBar from './Components/Shared/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- StWarStudios.Web.Server/web-client/src/Components/homepage.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/homepage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-import './HomePage.css';
+import './homepage.css';
 
 
 const HomePage = () => {
