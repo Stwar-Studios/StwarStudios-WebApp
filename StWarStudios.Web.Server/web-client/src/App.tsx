c99ff1b9b53e36cab37ca643fa512c9cfd8e03f5
Michael.Patino <michael.patino@stwarstudios.com>
2024-10-09 10:58:23 -0500

    fix navbar

--- StWarStudios.Web.Server/web-client/src/App.tsx
+++ StWarStudios.Web.Server/web-client/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import 'reset-css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Components/homepage';
-import NavBar from './Components/Shared/navbar';
+import NavBar from './Components/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
