d9be6f6f260eb85ba8637a5456906e36c65a3373
patycrack <patricia.osuna@stwarstudio.com>
2024-10-15 16:47:36 -0700

    I fixed the Contact button.

--- StWarStudios.Web.Server/web-client/src/Components/navbar.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/navbar.tsx
@@ -6,50 +6,52 @@ import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
 import { NavLink } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+
 function NavBar() {
     return (
         <header className='header'>
-        <div className='stwar-navbar'>
-            <Navbar bg="dark" data-bs-theme="dark" collapseOnSelect expand="lg" className=" p-1">
-                <Container className=''>
-                    <Navbar.Brand href="#home">
-                        <div className='d-flex align-items-center'>
-                            <Image src="images/logo.png" fluid width={68} />
-                            <div className='ms-2 text-center d-flex flex-column align-items-start'>
-                                <div className='stwar-title mb-0'><strong>STWAR</strong></div>
-                                <strong className='stwar-studios-title'>
-                                    <span> <small>Studios </small></span>
-                                </strong>
+            <div className='stwar-navbar'>
+                <Navbar bg="dark" data-bs-theme="dark" collapseOnSelect expand="lg" className="p-1">
+                    <Container>
+                        <Navbar.Brand href="#home">
+                            <div className='d-flex align-items-center'>
+                                <Image src="images/logo.png" fluid width={68} />
+                                <div className='ms-2 text-center d-flex flex-column align-items-start'>
+                                    <div className='stwar-title mb-0'><strong>STWAR</strong></div>
+                                    <strong className='stwar-studios-title'>
+                                        <span><small>Studios</small></span>
+                                    </strong>
+                                </div>
                             </div>
-                        </div>
-                    </Navbar.Brand>
+                        </Navbar.Brand>
 
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto w-100">
-                            <Container className='d-flex justify-content-center navbar-nav'>
-                                <NavLink to="/" 
-                                    className={({ isActive   }) => `p-2 navbar-item text-white ${isActive ? 'menu-item-border-primary' : ''}`}>
-                                    Home
-                                </NavLink >
-                                <NavLink to="/Services" className={({ isActive   }) => `p-2 navbar-item text-white ${isActive ? 'menu-item-border-primary' : ''}`}>
-                                    Servicios
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto w-100">
+                                <Container className='d-flex justify-content-center navbar-nav'>
+                                    <NavLink to="/" 
+                                        className={({ isActive}) => `p-2 navbar-item text-white ${isActive ? 'menu-item-border-primary' : ''}`}>
+                                        Home
+                                    </NavLink>
+                                    <NavLink to="/Services" 
+                                        className={({ isActive}) => `p-2 navbar-item text-white ${isActive ? 'menu-item-border-primary' : ''}`}>
+                                        Servicios
+                                    </NavLink>
+                                    <NavLink to="/Portafolio" 
+                                        className={({ isActive}) => `p-2 navbar-item text-white ${isActive ? 'menu-item-border-primary' : ''}`}>
+                                        Portafolio
+                                    </NavLink>
+                                </Container>
+                            </Nav>
+                            <Nav>
+                                <NavLink to='/Contact' className='contact-button1'>
+                                    <Button className='contact-button1'>Contact</Button>
                                 </NavLink>
-                                <NavLink to="/Portafolio" className={({ isActive   }) => `p-2 navbar-item text-white ${isActive ? 'menu-item-border-primary' : ''}`}>
-                                    Portafolio
-                                </NavLink>
-                            </Container>
-                        </Nav>
-                        <Nav>
-                            <NavLink to='/Contact' className='contact-button'>
-                            <Button className='btn-lg btn-warning text-white' >Contact</Button>
-                            
-                            </NavLink>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
         </header>
     );
 };
