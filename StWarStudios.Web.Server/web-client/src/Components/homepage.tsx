40810ea8e4e70de3fe64280081bdd7bbda50ba9f
patycrack <patricia.osuna@stwarstudio.com>
2024-10-18 11:11:09 -0700

    Fix responsive on home

--- StWarStudios.Web.Server/web-client/src/Components/homepage.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/homepage.tsx
@@ -45,19 +45,19 @@ const HomePage = () => {
         <div className="container">
           <h2 className="mb-5"><strong>Nuestros Servicios</strong></h2>
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-6">
               <div className="card service-card p-3 shadow-sm">
                 <img src="/images/keys.png" alt="soluciones" className="card-img-top" />
                 <div className="card-body">
-                  <h5 className="card-title">Soluciones <br /> Personalizadas</h5>
+                  <h5 className="card-title">Soluciones Personalizadas</h5>
                   <img src="/images/line blue.png" alt="LineBlue" style={{ width: '70%', height: 'auto', display: 'block', margin: '10px auto' }} />
                   <p className="card-text">Creación de software conforme a tus necesidades, digitalización de tus procesos para transformar tu negocio, incluyendo automatización de tareas manuales, y más.</p>
-                  <br />
+                 
                   <Button variant="primary" className="btn-blue w-100 mt-4">Más Información</Button> {/* Botón azul */}
                 </div>
               </div>
             </div>
-            <div className="col-md-4">
+            <div className="col-md-6">
               <div className="card service-card p-3 shadow-sm">
                 <img src="/images/cloud1.png" alt="integracion" className="card-img-top" />
                 <div className="card-body">
