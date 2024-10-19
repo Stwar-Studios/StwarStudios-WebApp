7e30c50de0ccb50fe5463e392d8cb5f5b00acee3
patycrack <patricia.osuna@stwarstudio.com>
2024-10-19 00:18:14 -0700

    - Import of Services Section CSS. - Design implemented to adjust the Cards. - Responsive setting.

--- StWarStudios.Web.Server/web-client/src/Components/home/services-section.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/home/services-section.tsx
@@ -1,8 +1,11 @@
 import Button from 'react-bootstrap/Button';
 import { Element } from 'react-scroll';
-function ServicesSection()
-{
- return <>
+import './services-section.css';
+
+function ServicesSection(){
+  
+  return <>
+  
 <Element name="services" className="services-section" >
         <div className="container">
           <h2 className="mb-5"><strong>Nuestros Servicios</strong></h2>
@@ -10,24 +13,23 @@ function ServicesSection()
             <div className="col-md-6">
               <div className="card service-card p-3 shadow-sm">
                 <img src="/images/keys.png" alt="soluciones" className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title">Soluciones Personalizadas</h5>
-                  <img src="/images/line blue.png" alt="LineBlue" style={{ width: '70%', height: 'auto', display: 'block', margin: '10px auto' }} />
-                  <p className="card-text">Creación de software conforme a tus necesidades, digitalización de tus procesos para transformar tu negocio, incluyendo automatización de tareas manuales, y más.</p>
-                 
-                  <Button variant="primary" className="btn-blue w-100 mt-4">Más Información</Button> {/* Botón azul */}
-                </div>
+                  <div className="card-body">
+                    <h5 className="card-title">Soluciones Personalizadas</h5>
+                      <img src="/images/line blue.png" alt="LineBlue" style={{ width: '70%', height: 'auto', display: 'block', margin: '10px auto' }} />
+                        <p className="card-text">Creación de software conforme a tus necesidades, digitalización de tus procesos para transformar tu negocio, incluyendo automatización de tareas manuales, y más.</p>  
+                          <Button variant="primary" className="btn-blue w-100 mt-4">Más Información</Button> {/* Button Blue */}
+                  </div>
               </div>
             </div>
             <div className="col-md-6">
               <div className="card service-card p-3 shadow-sm">
                 <img src="/images/cloud1.png" alt="integracion" className="card-img-top" />
-                <div className="card-body">
-                  <h5 className="card-title">Integración con <br /> Nuevas Tecnologías</h5>
-                  <img src="/images/line purple.png" alt="LinePurple" style={{ width: '70%', height: 'auto', display: 'block', margin: '10px auto' }} />
-                  <p className="card-text">Implementación y adaptación de tecnologías emergentes para modernizar tus sistemas existentes así como migración a la nube para escalamiento y mejora de la eficiencia operativa.</p>
-                  <Button className="btn-purple w-100 mt-4">Más Información</Button> {/* Botón morado */}
-                </div>
+                  <div className="card-body">
+                    <h5 className="card-title">Integración con <br /> Nuevas Tecnologías</h5>
+                      <img src="/images/line purple.png" alt="LinePurple" style={{ width: '70%', height: 'auto', display: 'block', margin: '10px auto' }} />
+                        <p className="card-text">Implementación y adaptación de tecnologías emergentes para modernizar tus sistemas existentes así como migración a la nube para escalamiento y mejora de la eficiencia operativa.</p>
+                          <Button className="btn-purple w-100 mt-4">Más Información</Button> {/* Button Purple */}
+              </div>
               </div>
             </div>
           </div>
