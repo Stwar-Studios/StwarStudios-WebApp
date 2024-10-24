1725882cfb8453dd4b175014f7aac99c69a22cca
patycrack <patricia.osuna@stwarstudio.com>
2024-10-23 17:36:26 -0700

    Modifying responsiveness and background image overlay.

--- StWarStudios.Web.Server/web-client/src/Components/Contact.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/Contact.tsx
@@ -4,39 +4,28 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Contact: React.FC = () => {
   return (
-    <>
-      {/* Sección de imagen de fondo */}
-      <div className="contact-section">
-        <section className="image-section">
-          <img src="/images/guys.jpg" alt="Fondo" className="background-image" />
-          <div className="image-overlay"></div>
-          <div className="image-text">
-            <span className="home-title">Lo increíble comienza aquí.</span><br />
-            <span className="home-sub-title mb-2 text-wrap">
-              Es momento de construir el futuro que una vez imaginaste.
-            </span>
-          </div>
-        </section>
+    <div className="contact-section">
+      <div className="title-container">
+        <h1 className="main-title">Lo increíble comienza hoy</h1>
+        <p className="main-subtitle">Es momento de construir el futuro que una vez te imaginaste.</p>
       </div>
-
-      {/* Sección del contenido con texto y formulario */}
-      <div className="form-container">
+      <section className="image-section">
+        <img src="/images/guys.jpg" alt="Fondo" className="background-image" />
+        <div className="image-overlay"></div>
         <div className="text-section">
-          <p className="contact-subtitle">Escríbenos</p>
-          <h1 className="contact-title">Ponte en contacto hoy</h1>
-          <p className="contact-description"><br />
-            Déjanos tus datos y estaremos encantados de<br /> trabajar contigo. Nuestro equipo está siempre 
-            <br />atento y dispuesto a ofrecerte la mejor solución a <br />tus necesidades tecnológicas.
-          </p>
-           {/* Lista de íconos */}
-           <ul className="icon-list"><br />
+        <h2 className="contact-title">Ponte en contacto</h2>
+          <p className="contact-description">
+            Déjanos tus datos y estaremos encantados de trabajar contigo.<br />
+            Nuestro equipo está siempre atento y dispuesto a ofrecerte la mejor
+            solución a tus necesidades tecnológicas.
+            </p>
+            <br />
+          <ul className="icon-list">
             <li><i className="fas fa-user"></i> thenatureguardians@gmail.com</li>
             <li><i className="fas fa-phone"></i> 972 254-6073</li>
             <li><i className="fas fa-home"></i> 601 254-6073</li>
           </ul>
         </div>
-
-        {/* Formulario */}
         <div className="form-section">
           <form className="contact-form">
             <input type="text" placeholder="Your name" className="contact-input" />
@@ -47,10 +36,9 @@ const Contact: React.FC = () => {
             <button type="submit" className="contact-button1">Enviar</button>
           </form>
         </div>
-      </div>
-    </>
+        </section>
+    </div>
   );
 };
 
 export default Contact;
-
