8fa828e39f264f82219838b0d236152caf357111
patycrack <patricia.osuna@stwarstudio.com>
2024-10-30 12:24:39 -0700

    -ApiUrl arrangement. -Overlay added. -Success message.

--- StWarStudios.Web.Server/web-client/src/Components/Contact.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/Contact.tsx
@@ -1,48 +1,240 @@
 import './contact.css';
 import React, { useState } from "react";
-import ApiForm from "./home/ApiForm";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Element } from 'react-scroll';
 import { FaUser, FaWhatsapp } from 'react-icons/fa';
 
-
 const ContactComponent: React.FC = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    topic: '',
+    message: ''
+  });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    topic: '',
+    message: '',
+    api: '' 
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
   const [successMessage, setSuccessMessage] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "phone") {
+      const filteredValue = value.replace(/[^0-9]/g, ""); 
+      setFormData({ ...formData, [name]: filteredValue });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+
+    setErrors({ ...errors, [name]: '', api: '' });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phoneRegex = /^[0-9]{7,15}$/;
+
+    const newErrors = {
+      name: '',
+      email: '',
+      phone: '',
+      topic: '',
+      message: '',
+      api: ''
+    };
+
+    if (!formData.name) newErrors.name = "Por favor, escriba su Nombre.";
+    if (!formData.email) {
+      newErrors.email = "Por favor, escriba un Correo válido.";
+    } else if (!emailRegex.test(formData.email)) {
+      newErrors.email = "Formato de correo inválido.";
+    }
+    if (!formData.phone) {
+      newErrors.phone = "Por favor, escriba un Teléfono válido.";
+    } else if (!phoneRegex.test(formData.phone)) {
+      newErrors.phone = "El teléfono debe contener entre 7 y 15 dígitos.";
+    }
+    if (!formData.topic) newErrors.topic = "Por favor, escriba un Tema de interés.";
+    if (!formData.message) newErrors.message = "Por favor, escriba un Mensaje, queremos leerte.";
+
+    if (Object.values(newErrors).some(error => error)) {
+      setErrors(newErrors);
+      setIsLoading(false);
+      return;
+    }
+
+    const apiUrl = `${process.env.REACT_APP_API_URL}v1/api/contact`;
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (!response.ok) throw new Error('Error al enviar el formulario');
+
+      const data = await response.json();
+      console.log('Message sent successfully:', data.message);
+
+      setFormData({ name: '', email: '', phone: '', topic: '', message: '' });
+      setErrors({ name: '', email: '', phone: '', topic: '', message: '', api: '' });
+      setSuccessMessage("¡Enviado con éxito! Nos pondremos en contacto pronto.");
+      setTimeout(() => setSuccessMessage(''), 5000); 
+    } catch (error) {
+      console.error('Error:', error);
+      setErrors(prevErrors => ({
+        ...prevErrors,
+        api: "Hubo un problema al enviar el formulario. Por favor, inténtalo de nuevo."
+      }));
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Element name="contact-section" className="contact-section">
       <img src="/images/guys.jpg" alt="Fondo" className="background-image" />
       <div className="contact-overlay"></div>
       <div className="title-container">
-        <h1 className="main-title">Lo increíble comienza aqu&iacute;</h1>
+        <h1 className="main-title">Lo increíble comienza aquí</h1>
         <h2 className="main-subtitle">Es momento de construir el futuro que una vez te imaginaste.</h2>
       </div>
 
       <section className="contact-content">
-      <div className="text-section">            
-            <p>Escribenos</p>
-            <h2 className="contact-title">Ponte en contacto</h2>
-            <p className="contact-description">
-              Déjanos tus datos y estaremos encantados de trabajar contigo.<br />
-              Nuestro equipo está siempre atento y dispuesto a ofrecerte la mejor
-              solución a tus necesidades tecnológicas.
-            </p>
-            <br />
-            <ul className="icon-list">
-              <li> <FaUser />contact@stwarstudios.com</li>
-              <li> <FaWhatsapp  />(+57) 316 9352239</li>
-                  </ul>
+        <div className="text-section">
+          <p>Escríbenos</p>
+          <h2 className="contact-title">Ponte en contacto</h2>
+          <p className="contact-description">
+            Déjanos tus datos y estaremos encantados de trabajar contigo.<br />
+            Nuestro equipo está siempre atento y dispuesto a ofrecerte la mejor
+            solución a tus necesidades tecnológicas.
+          </p>
+          <br />
+          <ul className="icon-list">
+            <li> <FaUser /> contact@stwarstudios.com</li>
+            <li> <FaWhatsapp /> (+57) 316 9352239</li>
+          </ul>
+        </div>
+        <div className="form-section">
+          {successMessage ? (
+            <div className="success-message">
+              <i className="fas fa-check-circle"></i>
+              {successMessage}
             </div>
-            <div className="form-section">
-                {successMessage && (
-                    <div className="success-message">
-                        {successMessage}
-                    </div>
-                )}
-                <ApiForm onSuccess={setSuccessMessage} />
+          ) : (
+            <form className="contact-form" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name"
+                className="contact-input"
+                value={formData.name}
+                onChange={handleChange}
+              />
+              {errors.name && (
+                <div className="error-message">
+                  <i className="fas fa-exclamation-circle"></i>
+                  {errors.name}
+                </div>
+              )}
+
+              <input
+                type="email"
+                name="email"
+                placeholder="Your email"
+                className="contact-input"
+                value={formData.email}
+                onChange={handleChange}
+              />
+              {errors.email && (
+                <div className="error-message">
+                  <i className="fas fa-exclamation-circle"></i>
+                  {errors.email}
+                </div>
+              )}
+
+              <input
+                type="tel"
+                name="phone"
+                placeholder="Your phone number"
+                className="contact-input"
+                pattern="[0-9]*"
+                value={formData.phone}
+                onChange={handleChange}
+              />
+              {errors.phone && (
+                <div className="error-message">
+                  <i className="fas fa-exclamation-circle"></i>
+                  {errors.phone}
+                </div>
+              )}
+
+              <input
+                type="text"
+                name="topic"
+                placeholder="Topic"
+                className="contact-input"
+                value={formData.topic}
+                onChange={handleChange}
+              />
+              {errors.topic && (
+                <div className="error-message">
+                  <i className="fas fa-exclamation-circle"></i>
+                  {errors.topic}
+                </div>
+              )}
+
+              <textarea
+                name="message"
+                placeholder="Your message"
+                className="contact-textarea"
+                value={formData.message}
+                onChange={handleChange}
+              ></textarea>
+              {errors.message && (
+                <div className="error-message">
+                  <i className="fas fa-exclamation-circle"></i>
+                  {errors.message}
+                </div>
+              )}
+
+              {errors.api && (
+                <div className="error-message">
+                  <i className="fas fa-exclamation-circle"></i>
+                  {errors.api}
+                </div>
+              )}
+
+              <button type="submit" className="contact-send-form">
+                Enviar
+              </button>
+            </form>
+          )}
+          {isLoading && (
+            <div className="overlay">
+              <div className="loading-message">Enviando...</div>
             </div>
+          )}
+        </div>
       </section>
     </Element>
   );
 };
 
 export default ContactComponent;
+
+
