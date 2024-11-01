f4ca8ba897f0eae6b494f45410caf4c97220ea89
Michael.Patino <michael.patino@stwarstudios.com>
2024-10-31 23:55:54 -0500

    update

--- StWarStudios.Web.Server/Program.cs
+++ StWarStudios.Web.Server/Program.cs
@@ -40,6 +40,7 @@ namespace StWarStudios.Web.Server
                 options.AddPolicy("AllowReactApp-Dev", policy =>
                 {
                     policy.WithOrigins(reactAppOrigin)
+                        .WithOrigins("https://qa-stwarstudios.azurewebsites.net")
                           .AllowAnyHeader()
                           .AllowAnyMethod();
                 });
