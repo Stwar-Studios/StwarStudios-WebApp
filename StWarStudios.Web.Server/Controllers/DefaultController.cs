88de113758233c770e3ce667624745ac578a4d23
Michael.Patino <michael.patino@stwarstudios.com>
2024-10-31 21:25:58 -0500

    added default CTRLER

---
+++ StWarStudios.Web.Server/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace StWarStudios.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        public DefaultController(IConfiguration configuration)
+        {
+            this.ApiVersion = configuration.GetValue<string>("EnvRelease");
+            this.EnvRelease = configuration.GetValue<string>("EnvRelease");
+        }
+
+        public string? ApiVersion { get; }
+        public string? EnvRelease { get; }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok($"Api version: {ApiVersion}, Env Released: {EnvRelease}");
+        }
+    }
+}
