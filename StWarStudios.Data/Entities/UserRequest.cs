0f5ab64a6b12692e1cf10316977893c085ff9599
Stwar-Studios <michael.patino@stwarstudios.com>
2024-11-05 15:15:25 -0500

    Add ip traking (#31)
    * added
    
    * fixssue

---
+++ StWarStudios.Data/Entities/UserRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StWarStudios.Data.Entities
+{
+    public class UserRequest
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [MaxLength(20)]
+        public string? UserIp { get; set; }
+
+        public int Count{ get; set; } = 0;
+
+        public DateTime CreationDate { get; set; }
+
+        public Guid? ObjectId { get; set; }
+    }
+}
