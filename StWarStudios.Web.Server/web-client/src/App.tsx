45a5636e6148e549eba01709e4370ec612ec9c56
patycrack <patricia.osuna@stwarstudio.com>
2024-11-05 17:18:19 -0700

    Added TermsConditions route

--- StWarStudios.Web.Server/web-client/src/App.tsx
+++ StWarStudios.Web.Server/web-client/src/App.tsx
@@ -4,6 +4,7 @@ import 'reset-css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './Components/homepage';
 import NavBar from './Components/navbar';
+import TermsConditions from './Components/footer/terms-conditions';
 // import ContactComponent from './Components/Contact';
 // import Services from './Components/Services';
 // import Portafolio from './Components/Portafolio';
@@ -19,8 +20,9 @@ function App() {
             <Route path="/" element={<HomePage />} />
             {/* <Route path="/Services" element={<Services />} />
             <Route path="/Portafolio" element={<Portafolio />} />
-            <Route path="/Contact" element={<ContactComponent />} /> */}
-          </Routes>          
+            <Route path="/Contact" element={<ContactComponent />} /> */}     
+          <Route path="/terms-conditions" element={<TermsConditions />} /> 
+          </Routes>    
       </div>
       <Footer />     
     </>
