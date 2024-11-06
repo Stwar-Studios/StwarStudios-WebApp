83efc039e1ab545bc60aee8f35f03b6013d6686a
patycrack <patricia.osuna@stwarstudio.com>
2024-11-05 17:18:37 -0700

    Link path updated

--- StWarStudios.Web.Server/web-client/src/Footer.tsx
+++ StWarStudios.Web.Server/web-client/src/Footer.tsx
@@ -12,7 +12,7 @@ const Footer: React.FC = () => {
           </Col>
           <Col md={6} className="text-center text-md-right">
             <a href="https://superadmin.es/blog/hosting/plantilla-politica-de-privacidad/" className="footer-link">Política de Privacidad</a> | 
-            <a href="https://www.termsfeed.com/public/uploads/2021/12/sample-terms-conditions-agreement.pdf" className="footer-link">Términos y Condiciones</a>
+            <a href="./terms-conditions" className="footer-link">Términos y Condiciones</a>
           </Col>
         </Row>
       </Container>
