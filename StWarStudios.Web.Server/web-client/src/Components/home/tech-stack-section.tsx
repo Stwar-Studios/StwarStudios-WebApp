5c13e3861f4b5cb529291dae601afd36f3c52eec
Stwar-Studios <michael.patino@stwarstudios.com>
2024-11-10 16:43:44 -0500

    support multi lang (#34)
    * support multi lang
    
    * fix issue

--- StWarStudios.Web.Server/web-client/src/Components/home/tech-stack-section.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/home/tech-stack-section.tsx
@@ -5,50 +5,52 @@ import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css'; // Importa los estilos CSS de tippy.js
 import { SiMicrosoftazure, SiDotnet, SiBlazor, SiAngular, SiReact } from 'react-icons/si';
 import { FaDatabase } from 'react-icons/fa';
+import { useTranslations  } from '../translation-provider';
+import { followCursor } from 'tippy.js';
 
 function TechStackSection() {
+  const { techStackSection } = useTranslations();
+
   return (
-    <>
-      <Element name="tech-stack" className="tech-stack text-white">
-        <div className="container">
-          <h1 className='text-center mb-5'>Nuestro stack tecnol&oacute;gico</h1>
-          <div className="icons">
-            <Tippy content="Blazor" followCursor='horizontal' placement="top">
-              <div className='icon'>
-                <SiBlazor className="blazor" />
+    <Element name="tech-stack" className="tech-stack text-white">
+      <div className="container">
+        <h1 className='text-center mb-5'>{techStackSection.title}</h1>
+        <div className="icons">
+          <Tippy content={techStackSection.technologies.blazor} followCursor={true} placement="top" plugins={[followCursor]}>
+            <div className='icon'>
+              <SiBlazor className="blazor" />
+            </div>
+          </Tippy>
+          <Tippy content={techStackSection.technologies.azure} followCursor={true} placement="top" plugins={[followCursor]}>
+            <div className='icon'>
+              <SiMicrosoftazure className="azure" />
+            </div>
+          </Tippy>
+          <Tippy content={techStackSection.technologies.dotnet} followCursor={true} placement="top" plugins={[followCursor]}>
+            <div className='icon'>
+              <SiDotnet className="dotnet" />
+            </div>
+          </Tippy>
+          <Tippy content={techStackSection.technologies.react} followCursor={true} placement="top" plugins={[followCursor]}>
+            <div className='icon'>
+              <SiReact className="react" />
+            </div>
+          </Tippy>
+          <Tippy content={techStackSection.technologies.angular} followCursor={true} placement="top" plugins={[followCursor]}>
+            <div className='icon'>
+              <SiAngular className="angular" />
+            </div>
+          </Tippy>
+          <Tippy content={techStackSection.technologies.sql} followCursor={true} placement="top" plugins={[followCursor]}>
+            <div className='icon combined-icon'>
+              <div className='sql'>
+                <FaDatabase />
               </div>
-            </Tippy>
-            <Tippy content="Microsoft Azure" followCursor={true} placement="top">
-              <div className='icon'>
-                <SiMicrosoftazure className="azure" />
-              </div>
-            </Tippy>
-            <Tippy content=".NET" followCursor={true} placement="top">
-              <div className='icon'>
-                <SiDotnet className="dotnet" />
-              </div>
-            </Tippy>
-            <Tippy content="React" followCursor={true} placement="top">
-              <div className='icon'>
-                <SiReact className="react" />
-              </div>
-            </Tippy>
-            <Tippy content="Angular" followCursor={true} placement="top">
-              <div className='icon'>
-                <SiAngular className="angular" />
-              </div>
-            </Tippy>
-            <Tippy content="MS SQL / Azure SQL" followCursor={true} placement="top">
-              <div className='icon combined-icon'>
-                <div className='sql'>
-                  <FaDatabase />
-                </div>
-              </div>
-            </Tippy>
-          </div>
+            </div>
+          </Tippy>
         </div>
-      </Element>
-    </>
+      </div>
+    </Element>
   );
 }
 
--- StWarStudios.Web.Server/web-client/src/Components/home/values-desc-section.tsx
+++ StWarStudios.Web.Server/web-client/src/Components/home/values-desc-section.tsx
@@ -3,47 +3,41 @@ import { Element } from 'react-scroll';
 import { FaRegHeart, FaShieldAlt, FaRegHandshake, 
   FaHourglassHalf 
   } from 'react-icons/fa';
+import { useTranslations  } from '../translation-provider';
 
-  const valuesIconSize = 75;
 
+const valuesIconSize = 75;
+const iconMapping : any= {
+  shield: FaShieldAlt,
+  heart: FaRegHeart,
+  hourglass: FaHourglassHalf,
+  handshake: FaRegHandshake
+};
 function ValuesDescSection()
 {
- return <>
-    <Element name="values-desc" className="values-desc text-dark" > 
-        <div className="">
-          <h1 className='text-center'><strong>Nuestros valores</strong></h1>
-          <div className='desc-content'>
-            <div className='values-item'>            
-              <FaShieldAlt className='img' size={valuesIconSize} color="#6D31ED" title="Integridad" />
-              <h2><strong>Integridad</strong></h2>            
-              <div className='value-body'>
-                transparentes. Somos responsables y éticos en nuestras acciones, servicios y productos
+  const { valuesDescSection } = useTranslations(); 
+  const values = valuesDescSection.values as [];
+  return (
+    <Element name="values-desc" className="values-desc text-dark">
+      <div className="">
+        <h1 className='text-center'><strong>{valuesDescSection.title}</strong></h1>
+        <div className='desc-content'>
+          {values.map((val : {title:string, color : string, description : string, icon : string}, index) => {
+            const IconComponent = iconMapping[val.icon] || FaShieldAlt; // Si no hay icono, usar el por defecto
+
+            return (
+              <div className='values-item' key={index}>
+                <IconComponent className='img' size={valuesIconSize} color={val.color} title={val.title} />
+                <h2><strong>{val.title}</strong></h2>
+                <div className='value-body'>
+                  {val.description}
+                </div>
               </div>
-            </div>
-            <div className='values-item'>         
-                <FaRegHeart className='img' size={valuesIconSize} color="#f04cc4" title="Disciplina" />
-                <h2><strong>Pasi&oacute;n</strong></h2>
-              <div className='value-body'>
-                Dejamos nuestro corazón en cada tarea. Nos emocionan los códigos, el diseño y la planeación
-              </div>
-            </div>
-            <div className='values-item'>
-              <FaHourglassHalf className='img' size={valuesIconSize} color="#f9623e" title="Corazón Candado" />
-              <h2><strong>Disciplina</strong></h2>
-              <div className='value-body'>
-                Somos constantes, puntuales, enfocados y organizados en nuestras actividades.
-              </div>
-            </div>
-            <div className='values-item'>
-              <FaRegHandshake className='img' size={valuesIconSize} color="#3787cf" title="Colaboración" />   
-              <h2><strong>Compromiso</strong></h2>
-              <div className='value-body'>
-                Damos el todo por el todo de manera voluntaria y apasionada.
-              </div>
-            </div>
-          </div>   
+            );
+          })}
         </div>
-        </Element>
- </>
+      </div>
+    </Element>
+  );
 }
 export default ValuesDescSection;
