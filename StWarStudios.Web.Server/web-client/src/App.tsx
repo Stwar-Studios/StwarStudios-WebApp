647de2c4d134f43e72d5d336521d4ac0ea8168fb
patycrack <patricia.osuna@stwarstudio.com>
2024-11-21 16:21:58 -0700

    Services was imported.

--- StWarStudios.Web.Server/web-client/src/App.tsx
+++ StWarStudios.Web.Server/web-client/src/App.tsx
@@ -6,10 +6,10 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './Components/homepage';
 import NavBar from './Components/navbar';
 // import ContactComponent from './Components/Contact';
-// import Services from './Components/Services';
+import Services from './Components/Services';
 // import Portafolio from './Components/Portafolio';
 import Footer from './Footer'; 
-import AboutUs from './Components/about-us'; // Importar el componente AboutUs
+import AboutUs from './Components/about-us';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function App() {
@@ -19,8 +19,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* Aquí agregamos una ruta para AboutUs */}
-          <Route path="/about-us" element={<AboutUs />} /> {/* Ruta para AboutUs */}
+          <Route path="/about-us" element={<AboutUs />} /> 
+          <Route path="/services" element={<Services />} /> 
         </Routes>
         <Footer />
       </div>
